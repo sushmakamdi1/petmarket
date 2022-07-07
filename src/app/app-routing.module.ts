c4bf2368cd49773f7b93df3715697918eb7411a2
NikhilM01 <nikhilmuneshwar01@gmail.com>
2022-07-07 15:15:40 +0530

    initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,14 @@ const routes: Routes = [
   {
     path: '',
     loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+  },
+  {
+    path: 'auth-screen',
+    loadChildren: () => import('./auth-screens/auth-screen/auth-screen.module').then( m => m.AuthScreenPageModule)
+  },
+  {
+    path: 'intro',
+    loadChildren: () => import('./auth-screens/intro/intro.module').then( m => m.IntroPageModule)
   }
 ];
 @NgModule({
